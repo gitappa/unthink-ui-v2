761d3e4731f93c74e120382785bede5bfdb22645
Nowsath04 <nowsathnichu@gmail.com>
2026-05-29 13:08:50 +0530

    qr responsive and spinner

--- web-client-next/src/components/kiosk/QRsection.jsx
+++ web-client-next/src/components/kiosk/QRsection.jsx
@@ -7,12 +7,12 @@ const QRsection = ({
   subtitle = "Scan the QR code to join our loyalty program and start collecting points instantly.",
 }) => {
   return (
-    <div className="relative mt-7 lg:mt-12 bg-white rounded-[18px] p-5 sm:p-7 w-full shadow-xl">
-      <div className="w-full rounded-[14px]  flex flex-col lg:flex-row items-center lg:items-start gap-4">
+    <div className="relative mt-7 lg:mt-12 bg-white rounded-[18px] p-4 sm:p-7 w-full shadow-xl">
+      <div className="w-full rounded-[14px]  md:flex flex-col md:flex-row items-center md:items-start gap-4">
         {/* Left: icon + text */}
-        <div className="flex-1 flex flex-col items-center lg:items-start text-center lg:text-left">
+        <div className="flex-1 md:flex flex-col items-center lg:items-start text-center lg:text-left">
           <div className="mt-1 mb-1.5">
-            <div className="w-14 h-14 rounded-full bg-[#fff6e6] flex items-center justify-center shadow-md">
+            <div className="w-14 h-14 rounded-full bg-[#fff6e6] flex items-center m-auto justify-center shadow-md">
               <svg
                 viewBox="0 0 24 24"
                 width="22"
@@ -29,7 +29,7 @@ const QRsection = ({
           <h3 className="h2-kiosk leading-[34px] mt-3 mb-2 text-slate-900 font-semibold">
             {title}
           </h3>
-          <p className="p-kiosk text-slate-400 mb-2 max-w-[400px]">
+          <p className="p-kiosk text-slate-400 mb-2 m-auto md:m-0 max-w-[400px]">
             {subtitle}
           </p>
         </div>
@@ -37,14 +37,14 @@ const QRsection = ({
         {/* Right: QR + EARN ONLY
           On small screens this block appears first (left-aligned),
           on large screens it is shown on the right side. */}
-        <div className="flex-shrink-0 flex flex-col items-start  lg:items-center order-first lg:order-last">
-          <div className="w-[180px] h-[180px] sm:w-[220px] sm:h-[220px] rounded-[14px] bg-[#fbfdff] flex items-center justify-center shadow-md border border-[rgba(2,6,23,0.04)] p-4">
+        <div className="flex-shrink-0 lg:flex m-auto flex-col items-start md:mt-0  mt-2 lg:items-center order-first lg:order-last">
+          <div className="w-[160px] m-auto h-[160px] sm:w-[220px] sm:h-[220px] rounded-[14px] bg-[#fbfdff] flex items-center justify-center shadow-md border border-[rgba(2,6,23,0.04)] p-4">
             <img src="https://aurastage.unthink.ai/settings/build_qrcode/?page_url=https://unthink-ui-next-stage-ui-v2-314035436999.us-central1.run.app/collections/testing-product-detail-page-173081113277330" alt="Qr code" />
           </div>
 
-          <div className="flex items-center gap-2.5 mt-2 lg:mt-2">
+          <div className="flex items-center justify-center gap-2.5 mt-2 lg:mt-2 m-auto">
             <span className="w-[10px] h-[10px] rounded-full bg-emerald-500 ring-[3px] ring-emerald-100/20" />
-            <span className="text-emerald-500 font-semibold text-[13px]">
+            <span className="text-emerald-500   font-semibold text-[13px]">
               EARN ONLY
             </span>
           </div>
