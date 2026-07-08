d9d6b135152f52af1b06fe718750f03d3b4fdfcb
Nowsath04 <nowsathnichu@gmail.com>
2026-07-08 16:27:22 +0530

    a

--- web-client-next/src/components/kiosk/components/AuthInput.jsx
+++ web-client-next/src/components/kiosk/components/AuthInput.jsx
@@ -14,6 +14,7 @@ import {
 	CopyOutlined,
 } from "@ant-design/icons";
 import { KIOSK_LOGIN_CHANGE_EVENT } from "../../../constants/codes";
+import { message } from "antd";
 const KIOSK_LOGIN_STORAGE_KEY = "Kiosk-login";
 
 const INITIAL_COLLECTION_QR_STATE = {
