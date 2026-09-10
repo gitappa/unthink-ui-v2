6d74ef1e865b2043917ef94a20e32e46e39526a7
NowsathUnthink <nowsatha@unthink.ai>
2026-09-10 12:57:16 +0530

    height incvrease

--- web-client-next/src/pageComponents/kiosk/KioskHome.js
+++ web-client-next/src/pageComponents/kiosk/KioskHome.js
@@ -151,7 +151,7 @@ const KioskHome = ({ props }) => {
         </div>
       </div>
       <div
-        className={`lg:flex ${showTags === "Social Media" ? "items-start" : "items-center"} gap-3`}
+        className={`min-w[1025]:flex ${showTags === "Social Media" ? "items-start" : "items-center"} gap-3`}
       >
         {showTags === "Social Media" && (
           <>
